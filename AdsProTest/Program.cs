7bd0200a1cf5144dd90b16ac1bfca88730c744e6
Alexander Savelyev <xavierernandez@gmail.com>
2022-09-09 19:33:33 +0300

    first commit

---
+++ AdsProTest/Program.cs
@@ -0,0 +1,28 @@
+using AdsProTest;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var apiConfig = builder.Configuration.GetSection("ApiConfiguration").Get<ApiConfiguration>();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ApiConfiguration>(apiConfig);
+
+var app = builder.Build();
+
+app.UseRouting();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.Run();
