251675835c909e974eceb56de705646f275cd9e2
Alexander Savelyev <xavierernandez@gmail.com>
2022-09-09 19:41:02 +0300

    solution cleared

--- AdsProTest/ApiConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AdsProTest
-{
-    public class ApiConfiguration
-    {
-        public string RequestToken { get; init; } = default!;
-        public string[] PermittedOS { get; init; } = default!;
-    }
-}
